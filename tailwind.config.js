4bcf2d56b095a42861f7262e201e3be0537114ef
GotaKusuma <gotama@undiksha.ac.id>
2024-02-21 09:03:52 +0800

    skripsi

---
+++ tailwind.config.js
@@ -0,0 +1,24 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ['index.html', 'role.html',  'login.html', 'dashboard.html'], 
+  theme: {
+    container: {
+      center: true,
+      padding: '16px',
+    },
+    extend: {
+      colors: {
+        primary: '#082F49',
+        dashboard: 'D9D9D9',
+        dark: '#020617',
+        hover: '#7DD3FC',
+        loginregister: '#93c5fd',
+      },
+      screens: {
+        '2xl' : '1320px',
+      },
+    },
+  },
+  plugins: [],
+}
+
