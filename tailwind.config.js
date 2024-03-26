ddcc3e760ed828f1b09fcfb74e9923e475dfb545
GotaKusuma <gotama@undiksha.ac.id>
2024-03-27 03:23:57 +0800

    update nih

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,6 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ['index.html', 'role.html',  'login.html', 'dashboard.html'], 
+  content: ['index.html', 'role.html',  'login.html', 'produk.html', 'toko.html'], 
   theme: {
     container: {
       center: true,
